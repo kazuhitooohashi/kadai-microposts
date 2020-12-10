e4507e37d540aa5677f0a7149566385bc35e66b2
taro yamadai <i.like.chocopie.than.angelpie@gmail.com>
2020-12-10 08:55:16 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User; // 追加
+
 class UsersController extends Controller
 {
     public function index()
@@ -14,17 +16,6 @@ class UsersController extends Controller
         // ユーザ一覧ビューでそれを表示
         return view('users.index', [
             'users' => $users,
-        ]);
-    }
-
-    public function show($id)
-    {
-        // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
-
-        // ユーザ詳細ビューでそれを表示
-        return view('users.show', [
-            'user' => $user,
         ]);
     }
     
